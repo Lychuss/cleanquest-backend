61a5e39ff059c4ad696b3c0405d9c34fd0e969ce
Raphael San Juan <raphaelsanjuan6@gmail.com>
2026-07-25 15:16:37 +0800

    added all the file

---
+++ app/app.ts
@@ -0,0 +1,38 @@
+import express from "express";
+import type { Request, Response } from "express";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "../lib/auth.js";
+import cors from "cors";
+
+const app = express();
+
+const allowedOrigins = [
+    process.env.FRONTEND_URL,
+    "http://localhost:3000"
+].filter(Boolean) as string[];
+
+app.use(cors({
+    origin: (origin, callback) => {
+        const allowedOrigins = [
+            process.env.FRONTEND_URL,
+            "http://localhost:3000"
+        ];
+
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    credentials: true
+}));
+
+app.all("/api/auth/*", toNodeHandler(auth));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/health", (req: Request, res: Response) => {
+    res.json({ status: "Server is running!" });
+});
+
+export default app;
---
+++ app/src/middlewares/authentication.ts
@@ -0,0 +1,12 @@
+import { fromNodeHeaders } from "better-auth/node";
+import { auth } from "../../../lib/auth.js";
+import type { Response, Request, NextFunction } from "express";
+
+export const requireAuth = async (req: Request, res: Response, next: NextFunction) => {
+    const session = await auth.api.getSession({ headers: fromNodeHeaders(req.headers)});
+    
+    if(!session) return res.status(401).json({ error: "Unauthorzied", success: false });
+
+    req.user = session.user;
+    next();
+}
