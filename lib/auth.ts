69c20f4ce6990526bf3fcf92350f6844bef256b3
Raphael San Juan <raphaelsanjuan6@gmail.com>
2026-08-09 00:32:01 +0800

    just add things in the betterAuth function for sign up

--- lib/auth.ts
+++ lib/auth.ts
@@ -9,7 +9,11 @@ export const auth = betterAuth({
     baseURL: process.env.BETTER_AUTH_URL,
     trustedOrigins: ["http://localhost:3000"],
     database: prismaAdapter(prisma, {provider: "postgresql"}),
-    emailAndPassword: {enabled: true},
+    emailAndPassword: {
+            enabled: true,
+            autoSignIn: true,
+            maxPasswordLength: 12
+        },
     socialProviders: {
             google: 
                 {
