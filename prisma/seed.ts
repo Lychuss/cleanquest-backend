4941c35417c7b7a5173b95d2d2fcf040ddb22c3e
Lychuss <raphaelsanjuan6@gmail.com>
2026-08-10 17:55:30 +0800

    fix the prisma schema import

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "../generated/prisma/client.js";
 import { PrismaPg } from '@prisma/adapter-pg';
 
 export const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
