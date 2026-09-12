fbc4cdf17480c3f53e4a3fb6ca00141eb745aba1
Raphael San Juan <raphaelsanjuan6@gmail.com>
2026-09-12 12:49:57 +0800

    increase the image limit

--- app/app.ts
+++ app/app.ts
@@ -33,8 +33,8 @@ app.use(cors({
     credentials: true
 }));
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 app.all("/api/auth/*splat", toNodeHandler(auth));
 
