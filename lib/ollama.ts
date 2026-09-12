456dbbb65b6981158e09e2232d0f0f0c1c702713
Raphael San Juan <raphaelsanjuan6@gmail.com>
2026-09-12 12:06:47 +0800

    created a .env variable for ollama for public deployment

--- lib/ollama.ts
+++ lib/ollama.ts
@@ -1,3 +1,7 @@
 import { Ollama } from "ollama";
+import dotenv from "dotenv";
+dotenv.config();
 
-export const ollama = new Ollama();
+export const ollama = new Ollama({
+    host: process.env.OLLAMA_HOST!
+});
